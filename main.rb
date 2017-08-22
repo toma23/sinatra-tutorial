d466744b44f4580d6366b2fb4fb27d10bde9219c
Rino Toma <simple1423kosen@gmail.com>
2017-08-22 12:00:33 +0900

    複数のパラメータ

--- main.rb
+++ main.rb
@@ -9,8 +9,12 @@ end
 get '/about' do
     "about this site page"
 end
-=end
 
 get '/hello/:name' do |n|
     "hello #{n}"
+end
+=end
+
+get '/hello/:fname/?:lname?' do |f,l|
+    "hello #{f} #{l}"
 end
