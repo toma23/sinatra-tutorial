e5df07f3a440f492012cf41ecaeb8ca59a345bef
Rino Toma <simple1423kosen@gmail.com>
2017-08-23 15:33:12 +0900

    コーティングスタイルの統一

--- board.rb
+++ board.rb
@@ -3,31 +3,31 @@ require 'sinatra/reloader'
 require 'active_record'
 
 ActiveRecord::Base.establish_connection(
-    "adapter" => "sqlite3",
-    "database" => "./bbs.db"
+  "adapter" => "sqlite3",
+  "database" => "./bbs.db"
 )
 
 #エスケープの決まり文句
 helpers do
-    include Rack::Utils
-    alias_method :h, :escape_html   #escape_htmlをhで呼べるようにしてる
+  include Rack::Utils
+  alias_method :h, :escape_html   #escape_htmlをhで呼べるようにしてる
 end
 
 class Comment < ActiveRecord::Base
 end
 
 get '/' do
-    @comments = Comment.order("id desc").all
-    erb :index_db
+  @comments = Comment.order("id desc").all
+  erb :index_db
 end
 
 #ホームに入力された文字を登録
 post '/new' do
-   Comment.create({:body => params[:body]})
-   redirect '/'
+  Comment.create({:body => params[:body]})
+  redirect '/'
 end
 
 #コメントを削除する
 post '/delete' do
-    Comment.find(params[:id]).destroy
+  Comment.find(params[:id]).destroy
  end
